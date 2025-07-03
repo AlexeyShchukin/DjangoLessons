43517ad2d2ebf6a7b877d294c47f75f6ec20497b
AlexeyShchukin <shchukin.developer@gmail.com>
2025-07-03 10:37:36 +0200

    fix: Add gerne field in book model

--- src/library/models/book.py
+++ src/library/models/book.py
@@ -1,3 +1,6 @@
+from enum import StrEnum
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 LAN_CHOICES = [
@@ -7,9 +10,33 @@ LAN_CHOICES = [
 ]
 
 
+class Genre(StrEnum):
+    N_A = "N/A"
+    FICTION = 'Fiction',
+    NON_FICTION = 'Non-Fiction',
+    SCIENCE_FICTION = 'Science Fiction',
+    FANTASY = 'Fantasy',
+    MYSTERY = 'Mystery',
+    BIOGRAPHY = 'Biography'
+
+    @classmethod
+    def choices(cls):
+        return [(attr.name, attr.value) for attr in cls]
+
+
 class Book(models.Model):
     title = models.CharField(max_length=65)
     description = models.TextField(
+        null=True,
+        blank=True,
+    )
+    genre = models.CharField(
+        max_length=50,
+        choices=Genre.choices(),
+        default=Genre.N_A,
+    )
+    pages = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(10000)],
         null=True,
         blank=True,
     )
